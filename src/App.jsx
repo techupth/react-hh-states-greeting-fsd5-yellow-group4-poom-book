1dba5cf1db2f4621dd5738882b33f39b9d4b6aec
Supakit <book0970403815@hotmail.com>
2023-12-21 13:39:17 +0700

    feat : send assignment by book

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputMessage, setGreetingMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{inputMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setGreetingMessage("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => setGreetingMessage("Hi!")}>Hi!</button>
+        <button onClick={() => setGreetingMessage("你好!")}>你好!</button>
       </div>
     </div>
   );
